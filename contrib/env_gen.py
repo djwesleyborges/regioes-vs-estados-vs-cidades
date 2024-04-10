8d56f10b20a8719ced2c9302755f5953bbdcbe0f
Wesley Borges <djwesleyborges@gmail.com>
2024-04-10 08:37:52 -0300

    feat: Adicionado arquivo para criação de .env

---
+++ contrib/env_gen.py
@@ -0,0 +1,30 @@
+"""
+Python SECRET_KEY generator.
+"""
+import random
+
+chars = 'abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!?@#$%^&*()'
+size = 50
+secret_key = ''.join(random.sample(chars, size))
+
+chars = 'abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!?@#$%_'
+size = 20
+password = ''.join(random.sample(chars, size))
+
+CONFIG_STRING = (
+    """
+DEBUG=True
+DEV=True
+SECRET_KEY=%s
+ALLOWED_HOSTS=127.0.0.1,.localhost,0.0.0.0
+
+""".strip()
+    % (secret_key, password)
+)
+
+# Writing our configuration file to '.env'
+with open('.env', 'w') as configfile:
+    configfile.write(CONFIG_STRING)
+
+print('Success!')
+print('Type: cat .env')
