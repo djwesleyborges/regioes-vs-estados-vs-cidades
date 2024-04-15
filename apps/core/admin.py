35e85ed3acd43beb71da0a6f27726a8f6e2ef61b
rg3915 <regis42santos@gmail.com>
2024-04-15 01:22:44 -0300

    Ajustes no Admin.

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import Cidade, Estado, Regiao
 
+
+@admin.register(Estado)
+class EstadoAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'regiao')
+    search_fields = ('nome',)
+    list_filter = ('regiao',)
+
+
+@admin.register(Cidade)
+class CidadeAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'estado')
+    search_fields = ('nome',)
+    list_filter = ('estado',)
+
+
 admin.site.register(Regiao)
-admin.site.register(Estado)
-admin.site.register(Cidade)
