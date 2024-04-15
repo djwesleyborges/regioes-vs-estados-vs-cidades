11310d3018949e114891adc7988bddfd67c107f3
rg3915 <regis42santos@gmail.com>
2024-04-15 01:30:41 -0300

    Adiciona index2.html

--- apps/core/views/index.py
+++ apps/core/views/index.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 
+# def index(request):
+#     context = {'a': 'aaa'}
+#     return render(request=request, template_name='index.html', context=context)
+
+
 def index(request):
-    context = {'a': 'aaa'}
-    return render(request=request, template_name='index.html', context=context)
+    return render(request, 'index2.html')
