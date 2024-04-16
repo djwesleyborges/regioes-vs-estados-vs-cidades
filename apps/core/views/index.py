f5e829d589894ac0e37c3e9ad720b8a1d765fa3a
rg3915 <regis42santos@gmail.com>
2024-04-16 00:27:42 -0300

    Padroniza para uma única página e único JS.

--- apps/core/views/index.py
+++ apps/core/views/index.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
 
 
-# def index(request):
-#     context = {'a': 'aaa'}
-#     return render(request=request, template_name='index.html', context=context)
-
-
 def index(request):
-    return render(request, 'index2.html')
+    return render(request, 'index.html')
--- static/js/consumer.js
+++ static/js/consumer.js
@@ -1,84 +1,95 @@
 const getData = () => ({
-    regioes: [],
-    estados: [],
-    cidades: [],
-    regiaoSelecionada: null,
-    estadoSelecionado: null,
-    cidadeSelecionada: null,
-    regiao: '',
-    estado: '',
-    cidade: '',
+  regioes: [],
+  estados: [],
+  cidades: [],
 
+  init() {
+    this.getReset()
+  },
 
-    init() {
-        this.getRegioes();
-        this.getTodosEstados();
-        this.getTodasCidades();
-    },
-    getRegioes() {
-        axios.get('/api/v1/regiao')
-            .then(response => {
-                this.regioes = response.data
-            })
-            .catch(error => {
-                alert(`Error: ${error}`)
-            })
-    },
-    getEstados() {
-        const regiao_id = this.regiaoSelecionada;
-        axios.get(`/api/v1/regiao/${regiao_id}/estados/`)
-            .then(response => {
-                this.estados = response.data
-            })
-            .catch(error => {
-                alert(`Error: ${error}`)
-            })
-    },
-    getTodosEstados() {
-        axios.get(`/api/v1/estados/`)
-            .then(response => {
-                this.estados = response.data
-            })
-            .catch(error => {
-                alert(`Error: ${error}`)
-            })
-    },
-    getCidades() {
-        const estado_id = this.estadoSelecionado;
-        axios.get(`/api/v1/estado/${estado_id}/cidade/`)
-            .then(response => {
-                this.cidades = response.data
-            })
-            .catch(error => {
-                alert(`Error: ${error}`)
-            })
-    },
-    getTodasCidades() {
-        axios.get('/api/v1/cidades/')
-            .then(response => {
-                this.cidades = response.data
-            })
-    },
-    getEstadoPorCidade() {
-        const cidade_id = this.cidadeSelecionada;
-        axios.get(`/api/v1/cidade/${cidade_id}/estado/`)
-            .then(response => {
-                console.log(response.data[0].nome)
-                this.estado = response.data[0].nome
-                //this.estados = response.data;
-                this.getRegiaoPorEstado(response.data[0].id)
-            })
-            .catch(error => {
-                alert(`Error: ${error}`)
-            })
-    },
-    getRegiaoPorEstado(estado_id) {
-        axios.get(`/api/v1/estado/${estado_id}/regiao/`)
-            .then(response => {
-                this.regiao = response.data[0].nome
-            })
-            .catch(error => {
-                alert(`Error: ${error}`)
-            })
-    }
+  getReset() {
+    this.getRegioes()
+    this.getTodosEstados()
+    this.getTodasCidades()
+  },
+
+  getRegioes() {
+    axios.get('/api/v1/regiao')
+      .then(response => {
+        this.regioes = response.data
+      })
+      .catch(error => {
+        alert(`Error: ${error}`)
+      })
+  },
+
+  getEstados(regiao_id) {
+    // Filtra Estados
+    axios.get(`/api/v1/regiao/${regiao_id}/estados/`)
+      .then(response => {
+        this.estados = response.data
+      })
+      .catch(error => {
+        alert(`Error: ${error}`)
+      })
+    // Filtra Cidades
+    axios.get(`/api/v1/regiao/${regiao_id}/cidades/`)
+      .then(response => {
+        this.cidades = response.data
+      })
+      .catch(error => {
+        alert(`Error: ${error}`)
+      })
+  },
+
+  getTodosEstados() {
+    axios.get(`/api/v1/estados/`)
+      .then(response => {
+        this.estados = response.data
+      })
+      .catch(error => {
+        alert(`Error: ${error}`)
+      })
+  },
+
+  getCidades(estado_id) {
+    // Filtra Cidades
+    axios.get(`/api/v1/estado/${estado_id}/cidade/`)
+      .then(response => {
+        this.cidades = response.data
+        // Filtra Região
+        this.getRegiaoPorEstado(response.data[0].estado)
+      })
+      .catch(error => {
+        alert(`Error: ${error}`)
+      })
+  },
+
+  getTodasCidades() {
+    axios.get('/api/v1/cidades/')
+      .then(response => {
+        this.cidades = response.data
+      })
+  },
+
+  getEstadoPorCidade(cidade_id) {
+    axios.get(`/api/v1/cidade/${cidade_id}/estado/`)
+      .then(response => {
+        this.estados = response.data
+        this.getRegiaoPorEstado(response.data[0].id)
+      })
+      .catch(error => {
+        alert(`Error: ${error}`)
+      })
+  },
+
+  getRegiaoPorEstado(estado_id) {
+    axios.get(`/api/v1/estado/${estado_id}/regiao/`)
+      .then(response => {
+        this.regioes = response.data
+      })
+      .catch(error => {
+        alert(`Error: ${error}`)
+      })
+  }
 })
